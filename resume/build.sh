0983d29db88cf3dc72bb3a9b150eed8e669d924f
ted-vo <vtnhon.dk@gmail.com>
2024-01-26 08:16:23 +0000

    Deploying to gh-pages from @ ted-vo/ted-vo.github.io@ce1eee74dfd245d0393ee272ea129f63b7630b42 🚀

---
+++ resume/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+[ $(resume -V) ] ||
+	(
+		echo "resume cli not found. Installing...\n"
+		pnpm install -g resume-cli
+	)
+
+pnpm i
+
+resume export ../assets/pdf/resume.pdf --theme onepage-plus
+
+cp -f resume.json ../assets/json/resume.json
